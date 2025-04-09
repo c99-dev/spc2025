c713e0bba1c687c4375e0638a7c9ad06fb3407a8
c99-dev <c99@kakao.com>
2025-04-09 17:28:24 +0900

    todo: 마무리 필요함

---
+++ 3.JS/4.Canvas/snake.js
@@ -0,0 +1,95 @@
+window.onload = initialize;
+
+const GAME_SPEED = 200;
+const BLOCK_SIZE = 20;
+
+function initialize() {
+  canvas = document.querySelector('#myCanvas');
+  ctx = canvas.getContext('2d');
+
+  snake = {
+    body: [{ x: BLOCK_SIZE, y: BLOCK_SIZE }],
+    direction: 'right',
+  };
+
+  food = {
+    x: Math.floor(Math.random() * (canvas.width / BLOCK_SIZE)) * BLOCK_SIZE,
+    y: Math.floor(Math.random() * (canvas.height / BLOCK_SIZE)) * BLOCK_SIZE,
+  };
+
+  setupEventListeners();
+
+  setInterval(gameLoop, GAME_SPEED);
+}
+
+function setupEventListeners() {
+  document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+      case 'ArrowUp':
+        if (snake.direction !== 'down') snake.direction = 'up';
+        break;
+      case 'ArrowDown':
+        if (snake.direction !== 'up') snake.direction = 'down';
+        break;
+      case 'ArrowLeft':
+        if (snake.direction !== 'right') snake.direction = 'left';
+        break;
+      case 'ArrowRight':
+        if (snake.direction !== 'left') snake.direction = 'right';
+        break;
+    }
+  });
+}
+
+function gameLoop() {
+  moveSnake();
+  draw();
+}
+
+function drawFood() {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(food.x, food.y, BLOCK_SIZE, BLOCK_SIZE);
+}
+
+function drawSnake() {
+  ctx.fillStyle = 'green';
+  snake.body.forEach((segment) => {
+    ctx.fillRect(segment.x, segment.y, BLOCK_SIZE, BLOCK_SIZE);
+  });
+}
+
+function moveSnake() {
+  const head = { ...snake.body[0] };
+
+  switch (snake.direction) {
+    case 'up':
+      head.y -= BLOCK_SIZE;
+      break;
+    case 'down':
+      head.y += BLOCK_SIZE;
+      break;
+    case 'left':
+      head.x -= BLOCK_SIZE;
+      break;
+    case 'right':
+      head.x += BLOCK_SIZE;
+      break;
+  }
+
+  snake.body.unshift(head);
+
+  if (head.x === food.x && head.y === food.y) {
+    food.x =
+      Math.floor(Math.random() * (canvas.width / BLOCK_SIZE)) * BLOCK_SIZE;
+    food.y =
+      Math.floor(Math.random() * (canvas.height / BLOCK_SIZE)) * BLOCK_SIZE;
+  } else {
+    snake.body.pop();
+  }
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawSnake();
+  drawFood();
+}
