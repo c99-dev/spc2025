2237219f73ab2da412b8779fb48b5f62d6f99c6b
c99-dev <c99@kakao.com>
2025-04-18 10:18:56 +0900

    chore: 각각 나뉘었던 파일들을 합침

---
+++ 4.Python/0.problem_300/1~100.py
@@ -0,0 +1,46 @@
+# 1
+print("Hello World")
+
+# 2
+print("Mary's cosmetics")
+
+# 3
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 4
+print(r"C:\Windows")
+
+# 5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+
+# \n == 개행 \t == 탭
+
+# 6
+print("오늘은", "일요일")
+# 오늘은 일요일
+
+# 7
+print("naver", "kakao", "sk", "samsung", sep=";")
+
+# 8
+print("naver", "kakao", "sk", "samsung", sep="/")
+
+# 9
+print("first", end="")
+print("second")
+
+# 10
+print(5 / 3)
+
+# 11
+삼성전자 = 50000
+보유주식수 = 10
+총평가금액 = 삼성전자 * 보유주식수
+print(총평가금액)
+
+# 12
+삼전투자정보 = {"시가총액": 298000000000000, "현재가": 50000, "PER": 15.79}
+
+print(삼전투자정보)
+
+# 13
